b92ca43ed3362ce9abf0a86320b808fcdbbb8335
marcelomosl-rstar <marcelo.mosl12@gmail.com>
2024-06-21 19:21:32 -0300

    feat: initial commit

---
+++ Raiva/src/Test.cpp
@@ -0,0 +1,9 @@
+#include "test.h"
+#include <iostream>
+
+
+namespace Raiva {
+	void Raiva::Print() {
+		std::cout << "Welcome to Raiva Engine, brought to you by Marcelo Mosl" << std::endl;
+	}
+}
---
+++ sandbox/src/main.cpp
@@ -0,0 +1,12 @@
+
+namespace Raiva {
+	__declspec(dllimport) void Print();
+}
+
+
+int main() {
+	
+	Raiva::Print();
+
+	return 0;
+}
